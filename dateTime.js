e9379b7e36f05775922b766f562ce2ffa8f67863
Shivam3783 <98631859+Shivam3783@users.noreply.github.com>
2024-02-27 08:49:53 +0530

    Update dateTime.js

--- dateTime.js
+++ dateTime.js
@@ -8,6 +8,6 @@ module.exports = http.createServer((req, res) => {
   const formattedDate = currentDate.toISOString();
 
   res.write(`Current Date and Time: ${formattedDate}`);
-  res.write(`prac7 21162101019_shivam`);
+  res.write(`prac7 21162101019_shivam      changes done.`);
   res.end();
 });
